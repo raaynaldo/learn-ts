40ee17af47cda49a2de2761014ffe3ca7644af65
raaynaldo <raaynaldo@hotmail.com>
2021-03-31 16:13:31 -0400

    array.

--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
 const person: {
     name: string,
-    age: number
+    age: number,
+    hobbies: string[]
 } = {
     name: 'Raynaldo',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Hiking']
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
