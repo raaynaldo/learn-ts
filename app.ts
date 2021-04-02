867f4e4532810d2586028790d99292af1738fe7a
raaynaldo <raaynaldo@hotmail.com>
2021-04-02 00:28:23 -0400

    function as types.

--- app.ts
+++ app.ts
@@ -9,4 +9,10 @@ function printResult(num: number): void {
 
 printResult(add(5, 12));
 
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+
+console.log(combineValues(8, 8));
 // let someValue: undefined
