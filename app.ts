61e1a6425e76269038794379dc718562cb463fba
raaynaldo <raaynaldo@hotmail.com>
2021-04-02 00:04:33 -0400

    literal types.

--- app.ts
+++ app.ts
@@ -1,15 +1,25 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text'
+) {
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
+  if (
+    (typeof input1 === 'number' && typeof input2 === 'number') ||
+    resultConversion === 'as-number'
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
 }
 
-const combineAges = combine(30, 25);
+const combineAges = combine(30, 25, 'as-number');
 console.log(combineAges);
 
-const combineNames = combine('Ray', 'Seulgis');
+const combineStringAges = combine('30', '26', 'as-number');
+console.log(combineStringAges);
+
+const combineNames = combine('Ray', 'Seulgi', 'as-text');
 console.log(combineNames);
